25a96c423f22772406faddebac1491163e174401
Ben Stolovitz <ben@stolovitz.com>
2023-03-25 11:43:14 -0700

    react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://ben.stolovitz.com",
-  integrations: [image(), mdx()]
+  integrations: [image(), mdx(), react()]
 });
